db8581dfe850fd28595adc8239fa13705e86505c
Stefan Maetschke <stefanrm@au1.ibm.com>
2017-02-07 10:26:31 +1100

    url in setup.py updated

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class PyTest(TestCommand):
 setup(
     name='nutsml',
     version=nutsml.__version__,
-    url='https://github.ibm.com/aur/nuts-ml',
+    url='https://github.com/maet3608/nuts-ml',
     license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
     author='Stefan Maetschke',
     author_email='stefanrm@au1.ibm.com',
@@ -69,7 +69,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2 :: Only',
         'Natural Language :: English',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
