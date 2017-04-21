069ca8189eb2cfbf3ee31b594fe990ee4f903931
Stefan Maetschke <stefanrm@au1.ibm.com>
2017-04-21 14:33:54 +1000

    minor refactoring

--- nutsml/examples/cifar/cnn_predict.py
+++ nutsml/examples/cifar/cnn_predict.py
@@ -21,11 +21,10 @@ if __name__ == "__main__":
 
     names = load_names()
 
-    transform = TransformImage(0).by('rerange', 0, 255, 0, 1, 'float32')
+    rerange = TransformImage(0).by('rerange', 0, 255, 0, 1, 'float32')
     show_image = ViewImageAnnotation(0, 1, pause=1, figsize=(3, 3),
                                      interpolation='spline36')
     pred_batch = BuildBatch(BATCH_SIZE).by(0, 'image', 'float32')
-    id2name = Map(lambda i: names[i])
 
     print('loading network...')
     network = create_network()
@@ -34,6 +33,6 @@ if __name__ == "__main__":
     print('predicting...')
     samples = glob('images/*.png') >> Print() >> ReadImage(None) >> Collect()
 
-    predictions = (samples >> transform >> pred_batch >>
-                   network.predict() >> Map(ArgMax()) >> id2name)
+    predictions = (samples >> rerange >> pred_batch >>
+                   network.predict() >> Map(ArgMax()) >> Map(names.__getitem__))
     samples >> Get(0) >> Zip(predictions) >> show_image >> Consume()
