1bd086a39883ad780b2e5532e27be604f85fd35d
Stefan Maetschke <stefanrm@au1.ibm.com>
2017-06-01 14:45:38 +1000

    six dependency removed

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
     description='Flow-based data pre-processing for Machine Learning',
     install_requires=[
         'nutsflow >= 1.0.14',
-        'six >= 1.10.0',
         'pyyaml >= 3.12',
         'xlrd >= 1.0.0',
         'dplython >= 0.0.7',
