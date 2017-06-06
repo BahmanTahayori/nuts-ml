6efc75526dd21660ed62614941f63c8117407eda
Stefan Maetschke <stefanrm@au1.ibm.com>
2017-06-06 11:40:18 +1000

    sorting added

--- tests/nutsml/test_stratify.py
+++ tests/nutsml/test_stratify.py
@@ -7,20 +7,20 @@ import pytest
 
 import random as rnd
 
-from nutsflow import Collect
+from nutsflow import Collect, Sort
 from nutsml import Stratify
 
 
 def test_Stratify():
     samples = [('pos', 1), ('pos', 1), ('neg', 0)]
     stratify = Stratify(1, mode='up', rand=rnd.Random(0))
-    stratified = samples >> stratify >> Collect()
+    stratified = samples >> stratify >> Sort()
     assert stratified == [('neg', 0), ('neg', 0), ('pos', 1), ('pos', 1)]
 
     samples = [('pos1', 1), ('pos2', 1), ('pos3', 1), ('neg1', 0), ('neg2', 0)]
     stratify = Stratify(1, mode='downrnd', rand=rnd.Random(0))
-    stratified = samples >> stratify >> Collect()
-    assert stratified == [('neg2', 0), ('neg1', 0), ('pos2', 1), ('pos1', 1)]
+    stratified = samples >> stratify >> Sort()
+    assert stratified == [('neg1', 0), ('neg2', 0), ('pos1', 1), ('pos2', 1)]
 
     with pytest.raises(ValueError) as ex:
         samples >> Stratify(1, mode='invalid') >> Collect()
