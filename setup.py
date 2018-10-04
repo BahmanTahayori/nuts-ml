849e9b938be9b6eac0a69a883574149de148dd9e
Stefan Maetschke <stefanrm@au1.ibm.com>
2018-10-04 14:15:18 +1000

    require nutsflow 1.0.30

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     author_email='stefan.maetschke@gmail.com',
     description='Flow-based data pre-processing for Machine Learning',
     install_requires=[
-        'nutsflow >= 1.0.29',
+        'nutsflow >= 1.0.30',
         'pandas < 0.21.0',  # due Python 3.4 support!
         'six >= 1.10.0',
         'pyyaml >= 3.12',
