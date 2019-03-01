e8dee4f34d2d08bcf6ee32b218381ea9caf5018b
stefanrm@au1.ibm.com <backprop!5>
2019-03-01 15:03:10 +1100

    support for python 3.7 added - again

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Natural Language :: English',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
