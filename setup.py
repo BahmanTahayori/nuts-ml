85d226a716960fadcf22e7cc26b8f585fbf45abe
stefanrm@au1.ibm.com <backprop!5>
2019-05-02 13:02:20 +1000

    require nutsflow 1.0.33 at setup

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     author_email='stefan.maetschke@gmail.com',
     description='Flow-based data pre-processing for Machine Learning',
     install_requires=[
-        'nutsflow >= 1.0.31',
+        'nutsflow >= 1.0.33',
         # 'pandas < 0.21.0',  # Python 3.4 support!
         'pandas > 0.21.0',
         'six >= 1.10.0',
